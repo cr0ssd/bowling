5a1938e0708971e76a2560e5dfb2ed8f67bb2e26
Isaac Martínez <isaacmtztruji@gmail.com>
2025-02-23 22:37:02 -0600

    Add files via upload

---
+++ lib/bowling.ex
@@ -0,0 +1,5 @@
+defmodule Bowling do
+  def score(game) do
+
+  end
+end
---
+++ test/bowling_test.exs
@@ -0,0 +1,13 @@
+defmodule BowlingTest do
+  use ExUnit.Case
+
+  test "gutter game" do
+    game = List.duplicate([0, 0], 9) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 0
+  end
+
+  test "'all ones' game" do
+    game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
+    assert Bowling.score(game) == 20
+  end
+end
